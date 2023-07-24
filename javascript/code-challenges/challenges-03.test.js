c51c0dbed74f9fd3ab6372bb0c9def0aef37aaa5
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-07-24 09:38:07 -0500

    completed challenge 5

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -62,7 +62,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  let newArray = arr.filter(element => !forbiddenValues.includes(element));
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
