735fc5e8d35f4f52a8967e9c3657551f2f3daa75
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-07-26 19:56:56 -0500

    passed challenge 3

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -45,7 +45,7 @@ Write a function named joinArray that takes an array and joins all of the elemen
 ------------------------------------------------------------------------------------------------ */
 
 const joinArray = (arr) => {
-  // Solution code here...
+  return arr.join(' ');
 };
 
 /* ------------------------------------------------------------------------------------------------
