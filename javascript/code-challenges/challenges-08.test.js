fb0695780958fc8da2d071d3ae1793947836c9dd
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-19 09:22:12 -0500

    Pass challenge 6

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -122,7 +122,8 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 ------------------------------------------------------------------------------------------------ */
 
 const citiesAtoJ = (arr) => {
-  // Solution code here...
+  const reg = /^[A-J]/;
+  return arr.filter(element => reg.test(element));
 };
 
 /* ------------------------------------------------------------------------------------------------
