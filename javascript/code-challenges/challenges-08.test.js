618baf36ca1f73a78202b67156e4981ec13893c7
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-18 19:33:59 -0500

    Pass challenge 2

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -72,7 +72,8 @@ Write a function named containsW that takes in a string. This function should us
 ------------------------------------------------------------------------------------------------ */
 
 const containsW = (str) => {
-  // Solution code here...
+  let reg = /(w)/;
+  return reg.test(str);
 };
 
 /* ------------------------------------------------------------------------------------------------
