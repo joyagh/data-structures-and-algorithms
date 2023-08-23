09768c5e149c644e234b213dee82ee8505a1d3e8
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-22 21:13:45 -0500

    Pass challenge 4

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -72,8 +72,14 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
-
+  const totals = [];
+  hoursOpen.forEach(() => totals.push(0));
+  stores.forEach(store => {
+    store.forEach((hour, idx) => {
+      totals[idx] += hour;
+    });
+  });
+  return totals;
 };
 
 /* ------------------------------------------------------------------------------------------------
