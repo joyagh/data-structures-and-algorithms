65650ded36b2d586d887e99bf56a9b14fbf4ea81
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-09-11 15:34:38 -0500

    Pass Challenge 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -64,7 +64,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  return /^[^.][\w]+\.?[\w]*[^.]@\S+\.(com|net|org)$/.test(email);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
