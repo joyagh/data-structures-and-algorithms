ad951681c096b4abd4b7560a826e7e074c88ea8b
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-09-11 16:02:15 -0500

    Pass challenge 6

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -70,7 +70,7 @@ Write a function named allHappy that takes in an array of strings and returns a 
 ------------------------------------------------------------------------------------------------ */
 
 const allHappy = (arr) => {
-  // Solution code here...
+  return arr.every(str => str.includes(':)'));
 };
 
 /* ------------------------------------------------------------------------------------------------
