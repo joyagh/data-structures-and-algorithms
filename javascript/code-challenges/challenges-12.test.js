b7327910a4e47a2bbe3a9fa40684a7a06c83bb89
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-09-11 15:30:56 -0500

    Pass challenge 3

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -31,7 +31,7 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 ------------------------------------------------------------------------------------------------ */
 
 const validateWord = (word) => {
-  // Solution code here...
+  return /^[a-zA-Z]{5,10}$/.test(word);
 };
 
 /* ------------------------------------------------------------------------------------------------
